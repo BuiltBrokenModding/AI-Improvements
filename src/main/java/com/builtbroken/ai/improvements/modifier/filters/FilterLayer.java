77cdb31de02659147e35112cccd8b5c6c0562367
bl4ckscor3 <bl4ckscor33@gmail.com>
2022-05-21 11:04:07 +0200

    fix: NPE, fixes #21

--- src/main/java/com/builtbroken/ai/improvements/modifier/filters/FilterLayer.java
+++ src/main/java/com/builtbroken/ai/improvements/modifier/filters/FilterLayer.java
@@ -1,9 +1,9 @@
 package com.builtbroken.ai.improvements.modifier.filters;
 
-import net.minecraft.world.entity.Entity;
-
 import java.util.Arrays;
 import java.util.Comparator;
+
+import net.minecraft.world.entity.Entity;
 
 /**
  * Created by Dark(DarkGuardsman, Robert) on 8/2/2019.
@@ -21,14 +21,16 @@ public class FilterLayer extends FilterNode
 
     public void add(IFilterNode modifier)
     {
-        if (modifier != null)
+        if (modifier == null)
         {
-            if (size == nodes.length)
-            {
-                nodes = Arrays.copyOf(nodes, nodes.length + 5);
-            }
-            nodes[size++] = new FilterNode(modifier);
+            modifier = NoopNode.INSTANCE;
         }
+
+        if (size == nodes.length)
+        {
+            nodes = Arrays.copyOf(nodes, nodes.length + 5);
+        }
+        nodes[size++] = new FilterNode(modifier);
     }
 
     @Override
@@ -37,36 +39,41 @@ public class FilterLayer extends FilterNode
         FilterResult filterResult = action != null ? action.handle(entity) : FilterResult.FILTERED;
         if (filterResult != FilterResult.DID_NOTHING && nodes != null)
         {
+            //just to be safe
+            for (int i = 0; i < nodes.length; i++)
+            {
+                if(nodes[i] == null)
+                    nodes[i] = NoopNode.INSTANCE;
+            }
+
             for (int i = 0; i < nodes.length; i++)
             {
                 final FilterNode modifier = nodes[i];
-                if (modifier != null)
+
+                filterResult = modifier.handle(entity);
+                if (filterResult == FilterResult.FILTERED)
                 {
-                    filterResult = modifier.handle(entity);
-                    if (filterResult == FilterResult.FILTERED)
+                    //increase call count
+                    modifier.callCount += 1;
+
+                    //Resort and clear
+                    if (modifier.callCount >= (Integer.MAX_VALUE / 2))
                     {
-                        //increase call count
-                        modifier.callCount += 1;
-
-                        //Resort and clear
-                        if (modifier.callCount >= (Integer.MAX_VALUE / 2))
-                        {
-                            Arrays.sort(nodes, Comparator.comparingInt(m -> m.callCount));
-                            final int sum = Arrays.stream(nodes).mapToInt(m -> m.callCount).sum();
-                            Arrays.stream(nodes).forEach(m -> m.callCount = (int) Math.floor(m.callCount / (double) sum));
-                        }
-                        else if (i != 0 && modifier.callCount > nodes[i - 1].callCount)
-                        {
-                            final FilterNode m = nodes[i];
-
-                            //Move down
-                            nodes[i] = nodes[i - 1];
-
-                            //Move up
-                            nodes[i - 1] = m;
-                        }
-                        return filterResult;
+                        Arrays.sort(nodes, Comparator.comparingInt(m -> m.callCount));
+                        final int sum = Arrays.stream(nodes).mapToInt(m -> m.callCount).sum();
+                        Arrays.stream(nodes).forEach(m -> m.callCount = (int) Math.floor(m.callCount / (double) sum));
                     }
+                    else if (i != 0 && modifier.callCount > nodes[i - 1].callCount)
+                    {
+                        final FilterNode m = nodes[i];
+
+                        //Move down
+                        nodes[i] = nodes[i - 1];
+
+                        //Move up
+                        nodes[i - 1] = m;
+                    }
+                    return filterResult;
                 }
             }
         }
